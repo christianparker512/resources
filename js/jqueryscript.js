d2b35e07f21fcca1590165b973d21fc3ea61c2f3
Christian Parker <christianparker512@gmail.com>
2020-11-15 17:54:02 -0600

    jquery animations with fade in, out and toggle animations

--- js/jqueryscript.js
+++ js/jqueryscript.js
@@ -1,6 +1,18 @@
 $(function() {
     // jQuery goes here...
 
-    // Uncomment this line to fade out the red box on page load
-    $(".red-box").fadeOut(2000);
+    // // Uncomment this line to fade out the red box on page load
+    // $(".red-box").fadeOut(2000);
+    $(".red-box").fadeTo(2000, .2);
+    $(".green-box").fadeTo(2000, 0.5);
+    $(".blue-box").fadeTo(2000, 0.5);
+    // $(".red-box").fadeOut(fast);
+    $(".red-box").fadeIn(2000);
+    // $(".red-box").fadeOut(fast);
+    // $(".red-box").fadeOut(2000);
+    $(".green-box").fadeIn('slow');
+    // $(".red-box").fadeOut(fast);
+    $(".blue-box").fadeToggle();
+    $(".blue-box").fadeToggle();
+
 });
