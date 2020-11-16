887aa046797c480127d796b167c4d7ace8084dd8
Christian Parker <christianparker512@gmail.com>
2020-11-15 19:14:43 -0600

    jquery animation practice with lightbox in real world example

--- js/jqueryscript.js
+++ js/jqueryscript.js
@@ -24,11 +24,47 @@ $(function() {
     // $("p").slideDown(5000);
 
     // Jquery AnimationEffect
-    $('.blue-box2').animate({
-        "margin-left": "200px"},
-        1000);
-    $('.blue-box2').animate({
-            "margin-left": "0px"},
-        1000);
+    // $('.blue-box2').animate({
+    //     "margin-left": "200px"},
+    //     1000);
+    // $('.blue-box2').animate({
+    //         "margin-left": "0px"},
+    //     1000);
+
+    // animating many things at the same time
+    // $('.blue-box2').animate({
+    //     "margin-left": "200px",
+    //     "opacity": "0",
+    //     "height": "50px",
+    //     "width": "50px",
+    //     "marginTop": "25px"
+    // }, 1000);
+    // $('p').animate({
+    //     "margin-left": "200px",
+    //     "opacity": "0",
+    //     "height": "50px",
+    //     "width": "50px",
+    //     "marginTop": "25px",
+    //     "font-size": "50px"
+    // }, 1000);
+
+    // Callback function with an alert
+    // $('.red-box').fadeTo(1000, 0.2).fadeOut().delay(500).fadeIn();
+    // $('.green-box').delay(2000).fadeTo(1000, 0.5).fadeOut().delay(500).fadeIn();
+    // $('.blue-box1').delay(3000).fadeTo(1000, 0.8, function(){
+    //     alert("This is the callback: animation is finished");
+    // });
+
+    // // callback trickledown effect
+    // $('.red-box').fadeTo(1000, 0.2, function () {
+    //     $('.green-box').delay(2000).fadeTo(1000, 0.5, function() {
+    //         $('.blue-box1').delay(3000).fadeTo(1000, 0.0,function (){
+    //             alert("This is the callback: animation is finished");
+    //         });
+    //     });
+    // });
+
+    // fadein a lightbox fadein and fadeout
+    $('.lightbox').delay(500).fadeIn(1000);
 
 });
