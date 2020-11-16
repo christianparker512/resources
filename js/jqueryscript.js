fe50499e220db1b5b01c1dd3787be47a280870bd
Christian Parker <christianparker512@gmail.com>
2020-11-15 18:12:25 -0600

    jquery animations hide, show, slideUp and slideDown at different time variables

--- js/jqueryscript.js
+++ js/jqueryscript.js
@@ -3,16 +3,20 @@ $(function() {
 
     // // Uncomment this line to fade out the red box on page load
     // $(".red-box").fadeOut(2000);
-    $(".red-box").fadeTo(2000, .2);
-    $(".green-box").fadeTo(2000, 0.5);
-    $(".blue-box").fadeTo(2000, 0.5);
-    // $(".red-box").fadeOut(fast);
-    $(".red-box").fadeIn(2000);
-    // $(".red-box").fadeOut(fast);
-    // $(".red-box").fadeOut(2000);
-    $(".green-box").fadeIn('slow');
-    // $(".red-box").fadeOut(fast);
-    $(".blue-box").fadeToggle();
-    $(".blue-box").fadeToggle();
+    // $(".red-box").fadeTo(2000, .2);
+    // $(".green-box").fadeTo(2000, 0.5);
+    // $(".blue-box").fadeTo(2000, 0.5);
+    // // $(".red-box").fadeOut(fast);
+    // $(".red-box").fadeIn(2000);
+    // // $(".red-box").fadeOut(fast);
+    // // $(".red-box").fadeOut(2000);
+    // $(".green-box").fadeIn('slow');
+    // // $(".red-box").fadeOut(fast);
+    // $(".blue-box").fadeToggle();
+    // $(".blue-box").fadeToggle();
+    $(".blue-box1").hide(1000);
+    $(".blue-box1").show(2000);
+    $(".blue-box2").slideUp(2000);
+    $(".blue-box2").slideDown(2000);
 
 });
